672c8b6257d797286ed0fe508461db532217d220
zhanglinjie <zhanglinjie@pxn.one>
2022-05-05 17:29:24 +0800

    feat serverside TLS

--- test/grpc_gateway_test.exs
+++ test/grpc_gateway_test.exs
@@ -10,16 +10,18 @@ defmodule GrpcGatewayTest do
 
     IO.puts("request without token")
 
-    {:ok, channel} = GRPC.Stub.connect("localhost:4000")
+    {:ok, channel} = GRPC.Stub.connect("localhost:4000", cred: %{ssl: []})
     request = Helloworld.HelloRequest.new!(name: "John")
     {:error, _} = Helloworld.Greeter.Stub.say_hello(channel, request)
     GRPC.Stub.disconnect(channel)
 
     IO.puts("request with token")
 
-    {:ok, channel} = GRPC.Stub.connect("localhost:4000", headers: [{"x-token", token}])
+    {:ok, channel} =
+      GRPC.Stub.connect("localhost:4000", headers: [{"x-token", token}], cred: %{ssl: []})
+
     request = Helloworld.HelloRequest.new!(name: "John")
-    Helloworld.Greeter.Stub.say_hello(channel, request) |> IO.inspect()
+    {:ok, _} = Helloworld.Greeter.Stub.say_hello(channel, request)
     GRPC.Stub.disconnect(channel)
   end
 end
