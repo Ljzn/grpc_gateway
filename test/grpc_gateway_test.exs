c92677e555d629bd9672293277e0968cf9cc19c2
zhanglinjie <zhanglinjie@pxn.one>
2022-05-05 15:59:58 +0800

    fix

--- test/grpc_gateway_test.exs
+++ test/grpc_gateway_test.exs
@@ -10,12 +10,16 @@ defmodule GrpcGatewayTest do
 
     IO.puts("request without token")
 
-    GRPC.Stub.connect("localhost:4000")
-    |> IO.inspect()
+    {:ok, channel} = GRPC.Stub.connect("localhost:4000")
+    request = Helloworld.HelloRequest.new!(name: "John")
+    {:error, _} = Helloworld.Greeter.Stub.say_hello(channel, request)
+    GRPC.Stub.disconnect(channel)
 
     IO.puts("request with token")
+
     {:ok, channel} = GRPC.Stub.connect("localhost:4000", headers: %{"Authorization" => token})
     request = Helloworld.HelloRequest.new!(name: "John")
     {:ok, reply} = Helloworld.Greeter.Stub.say_hello(channel, request)
+    GRPC.Stub.disconnect(channel)
   end
 end
