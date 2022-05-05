60e25625657b0747433e04c4a61cddddf2a3aa11
zhanglinjie <zhanglinjie@pxn.one>
2022-05-05 14:48:40 +0800

    feat nginx config

--- test/grpc_gateway_test.exs
+++ test/grpc_gateway_test.exs
@@ -4,12 +4,12 @@ defmodule GrpcGatewayTest do
   test "greets the world" do
     token = "Bearer TOKEN"
 
-    {:ok, channel} = GRPC.Stub.connect("localhost:50051", headers: %{"authorization" => token})
-    request = Helloworld.HelloRequest.new!(name: "John")
-    {:ok, reply} = Helloworld.Greeter.Stub.say_hello(channel, request)
-
     # check nginx status
     System.cmd("systemctl", ["status", "nginx"])
     |> IO.inspect()
+
+    {:ok, channel} = GRPC.Stub.connect("localhost:443", headers: %{"authorization" => token})
+    request = Helloworld.HelloRequest.new!(name: "John")
+    {:ok, reply} = Helloworld.Greeter.Stub.say_hello(channel, request)
   end
 end
